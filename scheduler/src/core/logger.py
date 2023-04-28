e0f793c496c64dd48a0eb96dfe8d45d263b60b77
Arthur Lakiza <lakiza@gmail.com>
2023-04-28 12:03:31 +0300

    Сделал compose для старта всех сервисов

--- scheduler/src/core/logger.py
+++ scheduler/src/core/logger.py
@@ -10,15 +10,6 @@ LOGGING = {
         'verbose': {
             'format': LOG_FORMAT
         },
-        'default': {
-            '()': 'uvicorn.logging.DefaultFormatter',
-            'fmt': '%(levelprefix)s %(message)s',
-            'use_colors': None,
-        },
-        'access': {
-            '()': 'uvicorn.logging.AccessFormatter',
-            'fmt': "%(levelprefix)s %(client_addr)s - '%(request_line)s' %(status_code)s",
-        },
     },
     'handlers': {
         'console': {
@@ -26,29 +17,11 @@ LOGGING = {
             'class': 'logging.StreamHandler',
             'formatter': 'verbose',
         },
-        'default': {
-            'formatter': 'default',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-        },
-        'access': {
-            'formatter': 'access',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-        },
     },
     'loggers': {
         '': {
             'handlers': LOG_DEFAULT_HANDLERS,
             'level': settings.logging.level_root,
-        },
-        'uvicorn.error': {
-            'level': settings.logging.level_uvicorn,
-        },
-        'uvicorn.access': {
-            'handlers': ['access'],
-            'level': settings.logging.level_uvicorn,
-            'propagate': False,
         },
     },
     'root': {
